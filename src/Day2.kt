4e734c90ae5b6e1ad3fde60e48147f84bad10ec8
bwbuhse <bwbuhse@utexas.edu>
2019-12-07 12:28:55 -0600

    Finish Day2

--- src/Day2.kt
+++ src/Day2.kt
@@ -1,13 +1,28 @@
 import java.io.File
 
 fun main() {
-    val file = "day2_p1.txt"
+    val output = 19690720
 
+    for (i in 0..99) {
+        for (j in 0..99) {
+            if (IntCode(i, j) == output) {
+                println(100 * i + j)
+            }
+        }
+    }
+}
+
+fun IntCode(noun: Int, verb: Int): Int {
     // IntCode
-    val intCode = File(file).useLines { it.toList() }.flatMap { it.split(",") }.map { it.toInt() }.toMutableList()
+    val intCode = File("day2.txt").useLines { it.toList() }.flatMap { it.split(",") }.map { it.toInt() }.toMutableList()
+
+    // Set noun and verb
+    intCode[1] = noun
+    intCode[2] = verb
 
     // Boolean that is set to true once the 99 opcode is reached
     var stopped = false
+    var broken = false
 
     // Loop over the IntCode
     for (i in 0 until intCode.size step 4) {
@@ -15,8 +30,11 @@ fun main() {
             1 -> if (!stopped) intCode[intCode[i + 3]] = intCode[intCode[i + 1]] + intCode[intCode[i + 2]]
             2 -> if (!stopped) intCode[intCode[i + 3]] = intCode[intCode[i + 1]] * intCode[intCode[i + 2]]
             99 -> stopped = true
+            else -> if (!stopped) {
+                broken = true
+            }
         }
     }
 
-    println(intCode[0])
+    return intCode[0]
 }
