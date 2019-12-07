2b4be5987e6d02a8a8f31342775867dafbd12e92
bwbuhse <bwbuhse@utexas.edu>
2019-12-07 12:10:23 -0600

    Finish Day2 part 1

--- src/Day2.kt
+++ src/Day2.kt
@@ -1,6 +1,22 @@
+import java.io.File
 
 fun main() {
-    val file = "day2.txt"
+    val file = "day2_p1.txt"
 
+    // IntCode
+    val intCode = File(file).useLines { it.toList() }.flatMap { it.split(",") }.map { it.toInt() }.toMutableList()
 
+    // Boolean that is set to true once the 99 opcode is reached
+    var stopped = false
+
+    // Loop over the IntCode
+    for (i in 0 until intCode.size step 4) {
+        when (intCode[i]) {
+            1 -> if (!stopped) intCode[intCode[i + 3]] = intCode[intCode[i + 1]] + intCode[intCode[i + 2]]
+            2 -> if (!stopped) intCode[intCode[i + 3]] = intCode[intCode[i + 1]] * intCode[intCode[i + 2]]
+            99 -> stopped = true
+        }
+    }
+
+    println(intCode[0])
 }
