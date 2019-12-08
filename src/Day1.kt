4379bba4c65e3ab45fa8c2a586c86b95d9ce00d1
bwbuhse <bwbuhse@utexas.edu>
2019-12-08 16:33:43 -0600

    Finish Day 1 part 2

--- src/Day1.kt
+++ src/Day1.kt
@@ -3,6 +3,23 @@ import java.io.File
 fun main() {
     val file = "day1.txt"
 
+    // Part 1
     println(File(file).useLines { it.toList() }.map { it.toInt() }.sumBy { it / 3 - 2 })
+
+    // Part 2
+    val fuel = File(file).useLines { it.toList() }.map { it.toInt() }.map { it / 3 - 2 }.toMutableList()
+
+    for (i in 0 until fuel.size) {
+        var nextFuel = fuel[i] // This is the next one to calculate
+
+        while (nextFuel > 0) {
+            nextFuel = nextFuel / 3 - 2
+            if (nextFuel > 0) {
+                fuel[i] += nextFuel
+            }
+        }
+    }
+
+    println(fuel.sum()) // Should be less than 4944360
 }
 
