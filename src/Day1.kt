f736e3479d209a981517943783e42f0bd68eece8
bwbuhse <bwbuhse@utexas.edu>
2019-12-08 16:39:39 -0600

    Make Day 1 part 2 a little bit more functional

--- src/Day1.kt
+++ src/Day1.kt
@@ -7,19 +7,21 @@ fun main() {
     println(File(file).useLines { it.toList() }.map { it.toInt() }.sumBy { it / 3 - 2 })
 
     // Part 2
-    val fuel = File(file).useLines { it.toList() }.map { it.toInt() }.map { it / 3 - 2 }.toMutableList()
+    val fuel = File(file).useLines { it.toList() }.map { it.toInt() }.map {
+        var fuel = it / 3 - 2
 
-    for (i in 0 until fuel.size) {
-        var nextFuel = fuel[i] // This is the next one to calculate
+        var nextFuel = fuel // This is the next one to calculate
 
         while (nextFuel > 0) {
             nextFuel = nextFuel / 3 - 2
             if (nextFuel > 0) {
-                fuel[i] += nextFuel
+                fuel += nextFuel
             }
         }
-    }
 
-    println(fuel.sum()) // Should be less than 4944360
+        fuel    // Put it as the last line so that it gets mapped
+    }.sum()
+
+    println(fuel)
 }
 
