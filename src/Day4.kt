a34016f0647e1a2ddc13cb91dc329ae14a322dc5
bwbuhse <bwbuhse@utexas.edu>
2019-12-08 17:45:52 -0600

    Finish Day 4 part 2

--- src/Day4.kt
+++ src/Day4.kt
@@ -8,6 +8,11 @@ fun main() {
     val options = (range[0]..range[1]).toSet().filter { isNonDecreasing(it) && hasMatchingAdjacentDigits(it) }.count()
     println(options)
 
+    // Part 2
+    val options2 =
+        (range[0]..range[1]).toSet().filter { isNonDecreasing(it) && hasTwoMatchingAdjacentDigits(it) }.count()
+    println(options2)
+
 }
 
 // Lambda functions used
@@ -15,4 +20,7 @@ fun isNonDecreasing(num: Int): Boolean =
     num.toString().toCharArray().mapIndexed { index, c -> index == 0 || c >= num.toString().toCharArray()[index - 1] }.all { it }
 
 fun hasMatchingAdjacentDigits(num: Int): Boolean =
-    num.toString().toCharArray().mapIndexed { index, c -> index != 0 && c == num.toString().toCharArray()[index - 1] }.any { it }
+    num.toString().toCharArray().toList().groupBy { it }.map { it.value.size }.any { it >= 2 }
+
+fun hasTwoMatchingAdjacentDigits(num: Int): Boolean =
+    num.toString().toCharArray().toList().groupBy { it }.map { it.value.size }.any { it == 2 }
