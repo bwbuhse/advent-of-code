357d83b963ee70dfbefa4ffd4d6ec80f0ae082cb
bwbuhse <bwbuhse@utexas.edu>
2019-12-08 21:14:11 -0600

    Add default parameters to evaluateIntCode

--- src/IntCode.kt
+++ src/IntCode.kt
@@ -1,12 +1,12 @@
 import java.io.File
 
-fun evaluateIntCode(file: String, noun: Int, verb: Int): Int {
+fun evaluateIntCode(file: String, noun: Int = -1, verb: Int = -1): Int {
     // IntCode
     val intCode = File(file).useLines { it.toList() }.flatMap { it.split(",") }.map { it.toInt() }.toMutableList()
 
     // Set noun and verb
-    intCode[1] = noun
-    intCode[2] = verb
+    intCode[1] = if (noun != -1) noun else intCode[1]
+    intCode[2] = if (verb != -1) verb else intCode[2]
 
     // Boolean that is set to true once the 99 opcode is reached
     var stopped = false
