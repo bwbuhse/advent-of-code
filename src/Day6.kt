af8b6d6d851cd389058af4f8fb3c7b30b6ae2b2d
bwbuhse <bwbuhse@utexas.edu>
2019-12-10 14:54:19 -0600

    Finish Day 6 part 1

--- src/Day6.kt
+++ src/Day6.kt
@@ -1,5 +1,31 @@
 import java.io.File
+import java.util.*
 
 fun main() {
-    val intCode = File("day6.txt").useLines { it.toList() }.flatMap { it.split(",") }.map { it.toInt() }.toMutableList()
+    val directOrbits = File("day6.txt").useLines { it.toList() }
+        .map { it.split(")") }
+        .map { listOf(it[0], it[1]) }
+        .fold(emptyMap<String, Set<String>>(), { map, it ->
+            map + mapOf(it[0] to (map[it[0]]?.union(setOf(it[1])) ?: setOf(it[1])))
+        })
+
+    val orbitsToCheck = LinkedList<String>()
+    orbitsToCheck += "COM"
+
+    val allOrbits = mapOf("COM" to 0).toMutableMap()
+
+    while (orbitsToCheck.isNotEmpty()) {
+        val current = orbitsToCheck.poll()
+
+        directOrbits.getOrDefault(current, emptySet()).forEach {
+            orbitsToCheck += it
+            allOrbits[it] = allOrbits.getOrDefault(current, 0) + 1
+        }
+    }
+
+    val allOrbitsSum = allOrbits.values.sum()
+
+    println(directOrbits)
+    println(allOrbits)
+    println(allOrbitsSum)
 }
